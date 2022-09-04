f5256b106aff8be101603a410399d3bd160d72e9
sdono158 <111999534+sdono158@users.noreply.github.com>
2022-09-04 15:48:45 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,8 +1,8 @@
 /*   STEP 1   */
 export const leagueID = "863906737118429184"; // your league ID
 export const leagueName = "Unusual Suspects"; // your league name
-export const dues = 100; // (optional) used in template constitution page
-export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
+export const dues = 0; // (optional) used in template constitution page
+export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 /*   STEP 2   */
